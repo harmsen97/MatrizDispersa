f2d58bc73e2f7b1936c7aadbf3e5eba2bc66ef1a
Jeronimo Harmsen <54420156+harmsen97@users.noreply.github.com>
2019-09-05 15:52:44 -0500

    agregué un ejemplo al main

--- main.cpp
+++ main.cpp
@@ -12,11 +12,17 @@ int main()
                     {0 , 0 , 0 , 0 , 0 },
                     {0 , 2 , 2 , 1 , 0 }
             };
-
+        int anotherMatrix[4][5] =
+            {
+                    {0 , 0 , 5 , 0 , 1 },
+                    {0 , 0 , 8 , 9 , 0 },
+                    {0 , 0 , 0 , 0 , 0 },
+                    {0 , 6 , 5 , 12, 0 }
+            };
 
     MatrizDispersa md=MatrizDispersa(sparseMatrix);
 
-    md.suma(sparseMatrix);
+    md.suma(anotherMatrix);
 
     md.transposicion(sparseMatrix);
 
